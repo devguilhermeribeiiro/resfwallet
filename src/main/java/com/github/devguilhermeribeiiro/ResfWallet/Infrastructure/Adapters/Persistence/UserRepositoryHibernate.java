efbeb85faf66a0d2459cc23bfd873bab40296429
devgr <devguilhermeribeiro000@gmail.com>
2025-06-24 22:53:24 -0300

    Repository adapter

---
+++ src/main/java/com/github/devguilhermeribeiiro/ResfWallet/Infrastructure/Adapters/Persistence/UserRepositoryHibernate.java
@@ -0,0 +1,64 @@
+package com.github.devguilhermeribeiiro.ResfWallet.Infrastructure.Adapters.Persistence;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import com.github.devguilhermeribeiiro.ResfWallet.Application.Exceptions.NotFoundException;
+import com.github.devguilhermeribeiiro.ResfWallet.Domain.Entities.User;
+import com.github.devguilhermeribeiiro.ResfWallet.Domain.Ports.Outbound.UserRepositoryPort;
+
+public class UserRepositoryHibernate implements UserRepositoryPort {
+    private SessionFactory sessionFactory;
+
+    public UserRepositoryHibernate(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public Optional<User> findById(UUID id) {
+        try(Session session = sessionFactory.openSession()) {
+            return Optional.ofNullable(session.find(User.class, id));
+        }
+    }
+
+    @Override
+    public User save(User user) {
+        try(Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.persist(user);
+            transaction.commit();
+            return user;
+        }
+    }
+
+    @Override
+    public User update(User user) {
+        try(Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            User updatedUser = session.merge(user);
+            transaction.commit();
+            return updatedUser;
+        }
+    }
+
+    @Override
+    public byte deleteById(UUID id) {
+        try(Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            User user = session.find(User.class, id);
+            if (user.equals(null)) {
+                throw new NotFoundException("User not found");
+            }
+            session.remove(user);
+            transaction.commit();
+            
+            // 0 == OK, anyother == 1
+            return 0;
+        }
+    }
+    
+}
