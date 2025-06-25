ca0b4dffbe2030d66647ca0d521e212ba28ae3f4
devgr <devguilhermeribeiro000@gmail.com>
2025-06-25 00:19:41 -0300

    Setup manual DI and javalin routes with apiBuilder directive

--- src/main/java/com/github/devguilhermeribeiiro/ResfWallet/App.java
+++ src/main/java/com/github/devguilhermeribeiiro/ResfWallet/App.java
@@ -1,18 +1,42 @@
 package com.github.devguilhermeribeiiro.ResfWallet;
 
+import org.hibernate.SessionFactory;
+
+import com.github.devguilhermeribeiiro.ResfWallet.Application.Services.UserService;
+import com.github.devguilhermeribeiiro.ResfWallet.Domain.Ports.UserRepositoryPort;
+import com.github.devguilhermeribeiiro.ResfWallet.Infrastructure.Adapters.Persistence.UserRepositoryHibernate;
+import com.github.devguilhermeribeiiro.ResfWallet.Infrastructure.Adapters.Web.UserController;
+import com.github.devguilhermeribeiiro.ResfWallet.Infrastructure.Config.HibernateUtil;
+
 import io.javalin.Javalin;
+import static io.javalin.apibuilder.ApiBuilder.*;
 
 public class App {
-    public String getGreeting() {
+    public static String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
+        SessionFactory session = HibernateUtil.buildSessionFactory();
+        UserRepositoryPort userRepository = new UserRepositoryHibernate(session);
+        UserService userService = new UserService(userRepository);
+        UserController userController = new UserController(userService);
+
         Javalin app = Javalin.create(config -> {
             config.router.apiBuilder(() -> {
-                
+                path("/users", () -> {
+                    post(userController.createUser);
+                    path("{id}", () -> {
+                        get(userController.getUser);
+                        patch(userController.updateUser);
+                        delete(userController.deleteUser);
+                    });
+                });
             });
         });
-        System.out.println(new App().getGreeting());
+
+        app.start(8080);
+
+        System.out.println(App.getGreeting());
     }
 }
