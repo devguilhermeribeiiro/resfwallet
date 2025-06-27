b50ad05379f211b95c364b3fc7d019e2db8ad7e4
devgr <devguilhermeribeiro000@gmail.com>
2025-06-27 01:18:12 -0300

    Adding the entities to Session builder of hibernate

--- src/main/java/com/github/devguilhermeribeiiro/ResfWallet/Infrastructure/Config/HibernateUtil.java
+++ src/main/java/com/github/devguilhermeribeiiro/ResfWallet/Infrastructure/Config/HibernateUtil.java
@@ -3,13 +3,19 @@ package com.github.devguilhermeribeiiro.ResfWallet.Infrastructure.Config;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import com.github.devguilhermeribeiiro.ResfWallet.Domain.Entities.RealEstateFund;
+import com.github.devguilhermeribeiiro.ResfWallet.Domain.Entities.Transaction;
 import com.github.devguilhermeribeiiro.ResfWallet.Domain.Entities.User;
+import com.github.devguilhermeribeiiro.ResfWallet.Domain.Entities.Wallet;
 
 public class HibernateUtil {
     public static SessionFactory buildSessionFactory() {
         return new Configuration()
             .configure("hibernate.cfg.xml")
             .addAnnotatedClass(User.class)
+            .addAnnotatedClass(Wallet.class)
+            .addAnnotatedClass(RealEstateFund.class)
+            .addAnnotatedClass(Transaction.class)
             .buildSessionFactory();
     }
 }
